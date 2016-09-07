da2e162cb57b437202f9400cd3c2dc108571cabf
Sammie <310519804@qq.com>
2016-09-07 14:18:30 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,40 +1,7 @@
-
-
-{
-	"member":400,
-	"first_name":"John",
-	"last_name":"Sweaut",
-	"score":5023,
-	"date":"2016-09-10"
-}
-
-{
-	"member":401,
-	"first_name":"Ted",
-	"last_name":"Kuc",
-	"score":7063,
-	"date":"2016-09-10"
-}
-
-
-{
-	"member":402,
-	"first_name":"Jurry",
-	"last_name":"Sam",
-	"score":6593,
-	"date":"2016-09-10"
-}
-
-
-{
-	"member":403,
-	"first_name":"Brack",
-	"last_name":"Bama",
-	"score":9752,
-	"date":"2016-09-10"
-}
-
---
+ 
 
 
 {"member":400,"first_name":"John","last_name":"Sweaut","score":5023,"date":"2016-09-10"}
+{"member":401,"first_name":"Ted","last_name":"Kuc","score":7063,"date":"2016-09-10"}
+{"member":402,"first_name":"Jurry","last_name":"Sam","score":6593,"date":"2016-09-10"}
+{"member":403,"first_name":"Brack","last_name":"Bama","score":9752,"date":"2016-09-10"}
